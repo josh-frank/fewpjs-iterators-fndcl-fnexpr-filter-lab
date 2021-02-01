56bcd6fd70450d25a3c909a5f12c1d0d3dab3704
Josh Frank <eurosewusa@gmail.com>
2021-02-01 09:26:42 -0500

    commit tree 1b57bb45b82d6f413bbc0d5d45f139cae0422082

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching( drivers, name ) {
+    return drivers.filter( driver => driver.toUpperCase() === name.toUpperCase() );
+}
+
+function fuzzyMatch( drivers, search ) {
+    return drivers.filter( driver => driver.slice( 0, search.length ) === search );
+}
+
+function matchName( drivers, name ) {
+    let matches = [];
+    drivers.forEach( function( driver ) { 
+        if ( driver.name === name ) { matches.push( driver ); }
+    } );
+    return matches;
+}
